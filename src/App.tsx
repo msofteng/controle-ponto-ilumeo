695593e9268362a47592fa42574ec17299eb5750
msofteng <mathiaspedro9@gmail.com>
2024-12-21 22:34:58 -0300

    criando a estrutura inicial do aplicativo, adicionando o mantine UI, apexcharts e react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import logo from './logo.png';
+import { BrowserRouter, Routes, Route, Outlet } from "react-router";
 import './App.css';
+
+function AuthLayout() {
+    return (
+        <>
+            <div>layout autenticação</div>
+            <Outlet/>
+        </>
+    );
+}
+
+function Login() {
+    return (
+        <div>login</div>
+    );
+}
+
+function Register() {
+    return (
+        <div>cadastro</div>
+    );
+}
+
+function AppLayout() {
+    return (
+        <>
+            <div>layout SPA</div>
+            <Outlet/>
+        </>
+    );
+}
+
+function Home() {
+    return (
+        <div>página inicial</div>
+    );
+}
+
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route element={<AuthLayout />}>
+                    <Route path="login" element={<Login />} />
+                    <Route path="register" element={<Register />} />
+                </Route>
+
+                <Route element={<AppLayout />}>
+                    <Route index path="/" element={<Home />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
